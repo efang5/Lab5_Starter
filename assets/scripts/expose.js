b93e76c9297f7850f6cee5a0a7ad4e5b13f867c2
Ethan Fang <ethanfang@Ethans-MacBook-Pro-2.local>
2025-05-05 16:50:44 -0700

    assests to assets correction

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -14,15 +14,15 @@ function init() {
   hornSelect.addEventListener("change", (event) => {
     if (event.target.value == "air-horn") {
       hornPic.src = "assets/images/air-horn.svg";
-      audio.src = "assests/audio/air-horn.mp3";
+      audio.src = "assets/audio/air-horn.mp3";
     }
     else if (event.target.value == "car-horn") {
       hornPic.src = "assets/images/car-horn.svg";
-      audio.src = "assests/audio/car-horn.mp3";
+      audio.src = "assets/audio/car-horn.mp3";
     }
     else if (event.target.value == "party-horn") {
       hornPic.src = "assets/images/party-horn.svg";
-      audio.src = "assests/audio/party-horn.mp3";
+      audio.src = "assets/audio/party-horn.mp3";
     }
   });
 
@@ -30,16 +30,16 @@ function init() {
     let vol = event.target.value;
     audio.volume = (vol/100);
     if(vol == 0){
-      volImg.src = "assests/icons/volume-level-0.svg";
+      volImg.src = "assets/icons/volume-level-0.svg";
     }
     else if(vol < 33){
-      volImg.src = "assests/icons/volume-level-1.svg";
+      volImg.src = "assets/icons/volume-level-1.svg";
     }
     else if(vol < 67){
-      volImg.src = "assests/icons/volume-level-2.svg";
+      volImg.src = "assets/icons/volume-level-2.svg";
     }
     else{
-      volImg.src = "assests/icons/volume-level-3.svg";
+      volImg.src = "assets/icons/volume-level-3.svg";
     }
   });
 
