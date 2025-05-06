eed27d1332512ccc2f73bc108e98aee4dc74c13b
Ethan Fang <ethanfang@Ethans-MacBook-Pro-2.local>
2025-05-05 22:28:10 -0700

    explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,40 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  const voices = synth.getVoices();
+  const selection = document.getElementById("voice-select");
+  let voice;
+  const text = document.getElementById("text-to-speak");
+  let textToSpeak;
+  const button = document.querySelector("button");
+  let utterance;
+  const picture = document.querySelector("img");
+
+  selection.addEventListener("load", () => {
+    for(let i = 0; i < voices.length(); i++){
+      let opt = document.createElement("option");
+      opt.text = voices[i];
+      selection.add(opt);
+    }
+  });
+
+  selection.addEventListener("change", (event) => {
+    voice = event.target.value;
+  });
+
+  text.addEventListener("input", (event) => {
+    textToSpeak = event.target.value;
+  });
+
+  button.addEventListener("click", () => {
+    utterance = new SpeechSynthesisUtterance(textToSpeak);
+    utterance.voice = voice;
+    synth.speak(utterance);
+    picture.src = "assets/images/smiling-open.png";
+  });
+
+  synth.addEventListener("end", () => {
+    picture.src = "assets/images/smiling.png";
+  });
 }
