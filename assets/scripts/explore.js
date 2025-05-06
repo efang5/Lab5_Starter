365395235efba37085a35eb562935c92a44000ec
Ethan Fang <ethanfang@Ethans-MacBook-Pro-2.local>
2025-05-05 22:46:27 -0700

    selectionFix

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -15,9 +15,16 @@ function init() {
 
   selection.addEventListener("load", () => {
     for(let i = 0; i < voices.length(); i++){
-      let opt = document.createElement("option");
-      opt.text = voices[i];
-      selection.add(opt);
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      selection.appendChild(option);
     }
   });
 
